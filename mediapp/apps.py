f0527e839e4e1bcaa140d2008a3e8d1cce1b7ba9
Emad-ahmed <amadahmed1234678@gmail.com>
2022-03-08 19:40:17 +0600

    make it better

--- mediapp/apps.py
+++ mediapp/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MediappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mediapp'
+    const port = Process.env.PORT | | 3000
