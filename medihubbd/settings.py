b9bbb4b8660a6ae85b54e50ec58998617ae120a5
Emad-ahmed <amadahmed1234678@gmail.com>
2022-07-20 01:02:03 -0700

    chnges

--- medihubbd/settings.py
+++ medihubbd/settings.py
@@ -17,7 +17,7 @@ import os
 
 import django_heroku
 import dj_database_url
-from decouple import config
+# from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -147,7 +147,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "sajidikram1245@gmail.com"
-EMAIL_HOST_PASSWORD = "NewI_12345"
+EMAIL_HOST_USER = "mrinmoyshanto@gmail.com"
+EMAIL_HOST_PASSWORD = "wiqjcuyqwcbinoqo"
 
 django_heroku.settings(locals())
